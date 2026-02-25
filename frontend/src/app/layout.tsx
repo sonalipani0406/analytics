54b3277cf804e857ebdb5878b07b732809e26db2
harydhk7 <harydhk2@gmail.com>
2026-02-25 15:44:32 +0530

    test

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
